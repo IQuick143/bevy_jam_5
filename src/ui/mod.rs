dfffda1ca280118cd4da27059b325179ace9feac
IWonderWhatThisApiDoes <tomas.cajan@seznam.cz>
2024-07-25 18:29:34 +0200

    HUD UI draft

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,12 +3,14 @@
 // Unused utilities and re-exports may trigger these lints undesirably.
 #![allow(dead_code, unused_imports)]
 
+pub mod hud;
 pub mod interaction;
 pub mod palette;
 mod widgets;
 
 pub mod prelude {
 	pub use super::{
+		hud,
 		interaction::{InteractionPalette, InteractionQuery},
 		palette as ui_palette,
 		widgets::{Containers as _, Widgets as _},
@@ -18,5 +20,5 @@ pub mod prelude {
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-	app.add_plugins(interaction::plugin);
+	app.add_plugins((interaction::plugin, hud::plugin));
 }
