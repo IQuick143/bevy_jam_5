a2cdc0ee6b47d12d24b1b6cc6da458f643f9bdf9
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-02-14 17:00:03 +0100

    Rewrite path animation framework

--- src/game/animation.rs
+++ src/game/animation.rs
@@ -29,7 +29,7 @@ pub fn plugin(app: &mut App) {
 	);
 }
 
-#[derive(Debug, Clone, Copy, Default, Reflect)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Reflect)]
 pub enum RotationDirection {
 	#[default]
 	Clockwise,
@@ -45,56 +45,257 @@ impl From<CycleTurningDirection> for RotationDirection {
 	}
 }
 
-/// Component for enabling animation behaviour
-#[derive(Component, Debug, Clone, Copy, Default, Reflect)]
+/// Component for enabling path animations
+#[derive(Component, Debug, Clone, Default, Reflect)]
 pub struct AnimatedObject {
-	/// Which way the slerp should go.
-	pub rotation_direction: RotationDirection,
-	/// <0.0-1.0> percentage of the animation progress
-	pub progress: f32,
-	/// Center of rotation
-	pub cycle_center: Vec3,
-	/// The direction we're starting from, if None, the animation skips to the end
-	pub start_direction: Option<Dir2>,
-	pub start_magnitude: f32,
-	/// The direction we're ending on, if None, the animation cannot play
-	pub final_direction: Option<Dir2>,
-	pub final_magnitude: f32,
+	/// The position where the object should be when no animation plays
+	static_position: Vec2,
+	/// The segments of the path that the object should take
+	segments: std::collections::VecDeque<AnimationPathSegment>,
+	/// Combined length of all path segments
+	/// since the path was last modified
+	total_length: f32,
+	/// Combined length of all expired path segments
+	/// since the path was last modified
+	traversed_length: f32,
+	/// Progress on the full animation in [0, 1]
+	progress: f32,
+	/// Spatial progress through the first segment [0, 1]
+	first_segment_progress: f32,
 }
 
 impl AnimatedObject {
-	pub fn sample(&self) -> Option<Vec3> {
-		let target = self.final_direction?;
-		Some(if let Some(source) = self.start_direction {
-			let t = animation_easing_function(self.progress).clamp(0.0, 1.0);
-			let dir = {
-				let mut angle = Vec2::angle_to(*source, *target);
-				match self.rotation_direction {
-					RotationDirection::Clockwise => {
-						if angle > 0.0 {
-							angle -= TAU
-						}
-					}
-					RotationDirection::CounterClockwise => {
-						if angle < 0.0 {
-							angle += TAU
-						}
-					}
+	pub fn is_in_progress(&self) -> bool {
+		!self.segments.is_empty()
+	}
+
+	/// Advances the animation by a given fraction of its length
+	pub fn add_progress(&mut self, delta_progress: f32) {
+		self.progress += delta_progress;
+		// Progress corrected by the easing function
+		// Also relative distance [0, 1] covered so far
+		let distance_progress = animation_easing_function(self.progress);
+		// How far (by distance) we are into the first segment
+		let mut delta_distance = distance_progress * self.total_length - self.traversed_length;
+
+		while let Some(first_segment) = self.segments.front() {
+			log::info!("{self:?}");
+			let first_segment_length = first_segment.length();
+			// Correct for rounding errors by explicitly overriding at 1
+			let first_segment_completed =
+				first_segment_length <= delta_distance || self.progress >= 1.0;
+			if first_segment_completed {
+				// The whole segment has been animated, so remove it
+				// and move on to the next
+				self.traversed_length += first_segment_length;
+				delta_distance -= first_segment_length;
+				self.segments.pop_front();
+			} else {
+				// Stop in the middle of the segment
+				self.first_segment_progress = delta_distance / first_segment_length;
+				return;
+			}
+			log::info!("Dropping section");
+		}
+
+		// No segments are left to animate, so use static position
+		self.progress = 1.0;
+		self.total_length = 0.0;
+		self.traversed_length = 0.0;
+	}
+
+	/// Gets the current position on the path
+	pub fn sample(&self) -> Vec2 {
+		self.segments
+			.front()
+			.map(|x| x.sample(self.first_segment_progress))
+			.unwrap_or(self.static_position)
+	}
+
+	/// Adds another segment to the animation
+	///
+	/// This resets animation progress to zero.
+	/// If possible, the new segment is spliced into the last
+	/// existing segment.
+	pub fn add_segment(&mut self, segment: AnimationPathSegment) {
+		self.static_position = segment.end_position();
+		self.reset_progress();
+		self.append_segment(segment);
+	}
+
+	/// Restarts the remaining path as a new animation sequence
+	fn reset_progress(&mut self) {
+		if let Some(first_segment) = self.segments.front_mut() {
+			let shortened_first_segment = first_segment.cut(self.first_segment_progress);
+			let first_segment_length = first_segment.length();
+			let first_segment_remaining_length = shortened_first_segment.length();
+			self.total_length += first_segment_remaining_length - first_segment_length;
+			*first_segment = shortened_first_segment;
+		}
+		self.progress = 0.0;
+		self.first_segment_progress = 0.0;
+		self.total_length -= self.traversed_length;
+		self.traversed_length = 0.0;
+	}
+
+	/// Adds a path segment to current animation sequence
+	fn append_segment(&mut self, segment: AnimationPathSegment) {
+		if let Some(last_segment) = self.segments.pop_back() {
+			if let Some(new_last_segment) = segment.splice(&last_segment) {
+				let last_segment_length = last_segment.length();
+				let new_last_segment_length = new_last_segment.length();
+				self.total_length += new_last_segment_length - last_segment_length;
+				if new_last_segment_length >= Self::SKIP_SEGMENT_LENGTH_THRESHOLD {
+					self.segments.push_back(new_last_segment);
 				}
-				Rot2::radians(angle * t) * source
-			};
-			let magnitude = f32::lerp(self.start_magnitude, self.final_magnitude, t);
-			self.cycle_center + (magnitude * dir).extend(0.0)
+			} else {
+				self.segments.push_back(last_segment);
+				let added_length = segment.length();
+				self.total_length += added_length;
+				self.segments.push_back(segment);
+			}
 		} else {
-			self.cycle_center + (self.final_magnitude * target).extend(0.0)
-		})
+			let added_length = segment.length();
+			self.total_length += added_length;
+			self.segments.push_back(segment);
+		}
+	}
+
+	/// How long a path segment has to be in order to not be skipped
+	const SKIP_SEGMENT_LENGTH_THRESHOLD: f32 = 0.0001;
+}
+
+/// A simple segment of a path animation
+#[derive(Clone, Copy, Debug, Reflect)]
+pub enum AnimationPathSegment {
+	/// Linear path defined by its start and end points
+	Line(Vec2, Vec2),
+	/// Arc segment
+	CircleArc(CircleArcPathSegment),
+}
+
+impl AnimationPathSegment {
+	fn end_position(&self) -> Vec2 {
+		match self {
+			Self::Line(_, x) => *x,
+			Self::CircleArc(x) => x.center_point + Vec2::from_angle(x.final_angle) * x.radius,
+		}
+	}
+
+	fn length(&self) -> f32 {
+		match self {
+			Self::Line(x, y) => x.distance(*y),
+			Self::CircleArc(x) => (x.initial_angle - x.final_angle).abs() * x.radius,
+		}
+	}
+
+	fn sample(&self, progress: f32) -> Vec2 {
+		match self {
+			Self::Line(x, y) => x.lerp(*y, progress),
+			Self::CircleArc(x) => {
+				let current_angle = x.initial_angle.lerp(x.final_angle, progress);
+				x.center_point + Vec2::from_angle(current_angle) * x.radius
+			}
+		}
+	}
+
+	/// Constructs a new path segment that is what will remain
+	/// of this path segment after a given progression is elapsed
+	fn cut(&self, progress: f32) -> Self {
+		match self {
+			Self::Line(x, y) => Self::Line(x.lerp(*y, progress), *y),
+			Self::CircleArc(x) => {
+				let mut result = *x;
+				result.initial_angle = x.initial_angle.lerp(x.final_angle, progress);
+				Self::CircleArc(result)
+			}
+		}
+	}
+
+	/// How different can two path segments be to be still considered splicable
+	const SPLICE_DISTANCE_THRESHOLD: f32 = 0.0001;
+
+	/// Attempts to splice a segment into the previous one
+	/// ## Parameters
+	/// - `other` - the segment that precedes this one
+	/// ## Returns
+	/// Path segment equivalent to both arguments in a sequence,
+	/// sans backtracking, or [`None`] if they cannot be spliced
+	fn splice(&self, other: &Self) -> Option<Self> {
+		match (self, other) {
+			(&Self::Line(c, d), &Self::Line(a, b)) => {
+				if b.distance_squared(c) > Self::SPLICE_DISTANCE_THRESHOLD.powi(2) {
+					return None;
+				}
+				let normalized_dot = (b - a).normalize_or_zero().dot((c - d).normalize_or_zero());
+				if normalized_dot > Self::SPLICE_DISTANCE_THRESHOLD.powi(2) {
+					return None;
+				}
+				Some(Self::Line(a, d))
+			}
+			(Self::CircleArc(second), Self::CircleArc(first)) => {
+				if first.center_point.distance_squared(second.center_point)
+					> Self::SPLICE_DISTANCE_THRESHOLD.powi(2)
+				{
+					return None;
+				}
+				if (first.radius - second.radius).abs() > Self::SPLICE_DISTANCE_THRESHOLD {
+					return None;
+				}
+				if (first.final_angle - second.initial_angle).rem_euclid(TAU)
+					> Self::SPLICE_DISTANCE_THRESHOLD
+				{
+					return None;
+				}
+				let mut result = *first;
+				result.final_angle += second.final_angle - second.initial_angle;
+				Some(Self::CircleArc(result))
+			}
+			_ => None,
+		}
+	}
+}
+
+#[derive(Clone, Copy, Debug, Reflect)]
+pub struct CircleArcPathSegment {
+	/// Center of the arc
+	pub center_point: Vec2,
+	/// Radius of the arc
+	pub radius: f32,
+	/// Position angle of the start point
+	pub initial_angle: f32,
+	/// Position angle of the end point
+	///
+	/// This is always compared to the initial angle,
+	/// and the direction of animation is determined
+	/// by which angle is larger. This means it is possible for the path
+	/// to span more than a full circle. by putting in angles that differ
+	/// by more than 2 pi
+	pub final_angle: f32,
+}
+
+impl CircleArcPathSegment {
+	/// Calculates the value that should be put into [`CircleArcPathSegment::final_angle`]
+	/// in order to make a path that is up to a full circle in a specified direction
+	pub fn final_angle_from_single_rotation(
+		start: f32,
+		end: f32,
+		direction: RotationDirection,
+	) -> f32 {
+		if start >= end && direction == RotationDirection::CounterClockwise {
+			end + TAU
+		} else if start <= end && direction == RotationDirection::Clockwise {
+			end - TAU
+		} else {
+			end
+		}
 	}
 }
 
 fn animation_easing_function(t: f32) -> f32 {
 	// Quadratic ease-out.
-	// Looks better that a flat rotation, but is easier
-	// to chain like we do that a double-ended easing function
+	// Looks better than a flat rotation, but is easier
+	// to chain like we do than a double-ended easing function
 	if t > 0.5 {
 		1.0 - (1.0 - t).powi(2) * 4.0 / 3.0
 	} else {
@@ -196,7 +397,7 @@ fn listen_for_moves(
 			log::warn!("Target of RotateSingleCycle is not a cycle entity");
 			continue;
 		};
-		let location = transform.translation();
+		let location = transform.translation().xy();
 		let mut vertex_loop = get_flipped_wrapped_iterator(&vertices.0, event.0.direction);
 		let Some(mut last_id) = vertex_loop.next() else {
 			continue;
@@ -215,11 +416,10 @@ fn listen_for_moves(
 			continue;
 		};
 
-		let end_position = end_vertex_transform.translation();
+		let end_position = end_vertex_transform.translation().xy();
 
 		if let Some(mut animation) = animation {
-			let Some(&(start_vertex, center_of_rotation, direction)) =
-				permutation_map.get(&end_vertex)
+			let Some(&(start_vertex, center_point, direction)) = permutation_map.get(&end_vertex)
 			else {
 				// No movement, we can skip
 				continue;
@@ -230,31 +430,22 @@ fn listen_for_moves(
 				continue;
 			};
 
-			// If the animation hasn't finished playing, we give it a chance to catch up.
-			let sample = animation.sample();
-			#[allow(clippy::unnecessary_unwrap)]
-			// Clippy is annoying and it cannot be fixed cleanly
-			let start_vector = if sample.is_some() && animation.progress < 1.0 {
-				sample.unwrap()
-			} else {
-				start_vertex_transform.translation()
-			};
+			let start_position = start_vertex_transform.translation().xy();
+			let initial_angle = (start_position - center_point).to_angle();
+			let final_angle = (end_position - center_point).to_angle();
+			let radius = end_position.distance(center_point);
+			let adjusted_final_angle = CircleArcPathSegment::final_angle_from_single_rotation(
+				initial_angle,
+				final_angle,
+				direction.into(),
+			);
 
-			animation.rotation_direction = direction.into();
-
-			animation.progress = 0.0;
-			animation.cycle_center = center_of_rotation;
-			let (start_dir, start_magnitude) =
-				Dir2::new_and_length((start_vector - center_of_rotation).xy())
-					.unwrap_or((Dir2::X, 0.0));
-			let (end_dir, end_magnitude) = Dir2::new_and_length(
-				(end_vertex_transform.translation() - center_of_rotation).xy(),
-			)
-			.unwrap_or((Dir2::X, 0.0));
-			animation.start_direction = Some(start_dir);
-			animation.start_magnitude = start_magnitude;
-			animation.final_direction = Some(end_dir);
-			animation.final_magnitude = end_magnitude;
+			animation.add_segment(AnimationPathSegment::CircleArc(CircleArcPathSegment {
+				center_point,
+				radius,
+				initial_angle,
+				final_angle: adjusted_final_angle,
+			}));
 		} else {
 			// Object is not animated, so we just set the translation to the desired place.
 			transform.translation.x = end_position.x;
@@ -265,14 +456,13 @@ fn listen_for_moves(
 
 fn move_objects(mut objects: Query<(&mut Transform, &mut AnimatedObject)>, time: Res<Time<Real>>) {
 	for (mut transform, mut animation) in objects.iter_mut() {
-		if animation.progress >= 1.0 {
+		if !animation.is_in_progress() {
 			continue;
 		}
-		animation.progress += (time.delta_secs() / ANIMATION_TIME).min(1.0);
-		if let Some(goal) = animation.sample() {
-			transform.translation.x = goal.x;
-			transform.translation.y = goal.y;
-		}
+		animation.add_progress(time.delta_secs() / ANIMATION_TIME);
+		let new_position = animation.sample();
+		transform.translation.x = new_position.x;
+		transform.translation.y = new_position.y;
 	}
 }
 
@@ -342,6 +532,9 @@ fn init_cycle_animation(
 
 fn init_thing_animation(mut commands: Commands, query: Query<Entity, Added<ThingData>>) {
 	for id in &query {
+		// We do not need to initialize its static position
+		// since the animation does not get queried until
+		// something actually animates
 		commands.entity(id).insert(AnimatedObject::default());
 	}
 }
