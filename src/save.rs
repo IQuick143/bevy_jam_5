b1c990cd81fc31437fd0781b6ff55227d1370c77
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-05-20 23:28:08 +0200

    Structure completion list as an array

--- src/save.rs
+++ src/save.rs
@@ -1,4 +1,4 @@
-use bevy::{app::App, ecs::resource::Resource, platform::collections::HashMap};
+use bevy::{app::App, ecs::resource::Resource, platform::collections::HashSet};
 
 use crate::persistent::{register_saveable_resource, Saveable};
 
@@ -6,77 +6,56 @@ pub fn plugin(app: &mut App) {
 	register_saveable_resource::<SaveGame>(app);
 }
 
+/// Persistent state of a game
 #[derive(Resource, Default)]
 pub struct SaveGame {
-	completed_levels: HashMap<String, bool>,
+	completed_levels: HashSet<String>,
 }
 
 /// String identifier for the map storign the completed levels
 const COMPLETED_LEVELS: &str = "completed_levels";
 
-fn get_mut_or_create_map<'a>(
-	map: &'a mut serde_json::Map<String, serde_json::Value>,
-	key: &str,
-) -> &'a mut serde_json::Map<String, serde_json::Value> {
-	if !map.contains_key(key) {
-		map.insert(
-			key.to_string(),
-			serde_json::Value::Object(serde_json::Map::new()),
-		);
-	}
-	match map.get_mut(key) {
-		Some(serde_json::Value::Object(dict)) => dict,
-		Some(other) => {
-			*other = serde_json::Value::Object(serde_json::Map::new());
-			other.as_object_mut().unwrap()
-		}
-		None => unreachable!(),
-	}
-}
-
 impl Saveable for SaveGame {
 	const FILENAME: &str = "save";
 
 	fn write_json(&self, store: &mut serde_json::Value) {
-		use serde_json::Map;
-		use serde_json::Value::*;
+		use serde_json::{Map, Value};
 		if !store.is_object() {
-			*store = Object(Map::new());
+			*store = Value::Object(Map::new());
 		}
-		let Object(store_dict) = store else {
-			unreachable!()
-		};
-		let completed_levels = get_mut_or_create_map(store_dict, COMPLETED_LEVELS);
-		for (level, completion) in self.completed_levels.iter() {
-			completed_levels.insert(level.to_string(), Bool(*completion));
-		}
+		let store_dict = store.as_object_mut().unwrap();
+		let arr = self
+			.completed_levels
+			.iter()
+			.map(String::to_owned)
+			.map(Value::String)
+			.collect();
+		store_dict.insert(COMPLETED_LEVELS.to_owned(), Value::Array(arr));
 	}
 
 	fn read_json(&mut self, store: &serde_json::Value) {
-		use serde_json::Value::*;
-		let Object(main_map) = store else {
-			return;
-		};
-		if let Some(Object(completed_levels)) = main_map.get(COMPLETED_LEVELS) {
-			for (level_name, value) in completed_levels.iter() {
-				// If it's not a true bool, we pretend we didn't see that
-				if let Bool(value) = value {
-					self.completed_levels.insert(level_name.clone(), *value);
-				}
-			}
-		}
+		let completed_levels = store
+			.as_object()
+			.and_then(|m| m.get(COMPLETED_LEVELS))
+			.and_then(serde_json::Value::as_array)
+			.into_iter()
+			.flatten()
+			.filter_map(serde_json::Value::as_str)
+			.map(str::to_owned);
+		self.completed_levels = HashSet::from_iter(completed_levels);
 	}
 }
 
 impl SaveGame {
-	#[expect(dead_code)]
-	fn is_level_completed(&self, level_id: &str) -> bool {
-		*self.completed_levels.get(level_id).unwrap_or(&false)
+	pub fn _is_level_completed(&self, level_id: &str) -> bool {
+		self.completed_levels.contains(level_id)
 	}
 
-	#[expect(dead_code)]
-	fn set_level_completion(&mut self, level_id: &str, completion: bool) {
-		self.completed_levels
-			.insert(level_id.to_string(), completion);
+	pub fn set_level_completion(&mut self, level_id: &str, completion: bool) {
+		if completion {
+			self.completed_levels.insert(level_id.to_owned());
+		} else {
+			self.completed_levels.remove(level_id);
+		}
 	}
 }
